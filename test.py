4f790b83d480546838e82d650b3af2683b394fd2
epicrunze <epicrunze@gmail.com>
2021-05-09 02:28:47 -0400

    removed old files

--- test.py
+++
@@ -1,6 +0,0 @@
-with open("foods.txt", "r") as f:
-    with open("output.txt", "w") as w:
-        out = []
-        for line in f.readlines():
-            out.append(line.strip())
-        w.write(str(out))
